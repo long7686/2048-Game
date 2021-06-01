722fe85219e7057b20c43d2b847b43b04e76b74f
long7686 <thlong206@gmail.com>
2021-06-01 17:43:08 +0700

    no message

--- library/imports/e7/e74a8138-1c3e-4af7-b0ff-f5de883aadec.js
+++ library/imports/e7/e74a8138-1c3e-4af7-b0ff-f5de883aadec.js
@@ -2,7 +2,7 @@
 cc._RF.push(module, 'e74a8E4HD5K97D/9d6IOq3s', 'BlockController');
 // Scripts/BlockController.js
 
-"use strict";
+'use strict';
 
 var colors = require("Colors");
 
@@ -12,11 +12,14 @@ cc.Class({
     properties: {
         titleBlock: {
             default: null,
-            type: cc.Label
+            type: cc.Label,
+            _canCombine: false
         }
     },
 
-    onLoad: function onLoad() {},
+    onLoad: function onLoad() {
+        this._canCombine = false;
+    },
     setNumber: function setNumber(number) {
         if (number === 0) {
             this.titleBlock.node.active = false;
@@ -24,20 +27,18 @@ cc.Class({
         } else {
             this.titleBlock.string = number;
             this.titleBlock.node.active = true;
+            this.titleBlock.node.color = cc.Color.WHITE;
             if (number in colors) {
                 this.node.color = colors[number];
             }
+            if (number == 2) {
+                this.titleBlock.node.color = cc.color('#635B52');
+            }
+            if (number == 4) {
+                this.titleBlock.node.color = cc.color('#635B52');
+            }
         }
     }
-}
-
-// getRandomTitle(){
-//     let titleNum = null;
-//     Math.random() <= 0.95 ? titleNum = 2 : titleNum = 4;
-//     return this.titleBlock.string = titleNum;
-// }
-
-
-);
+});
 
 cc._RF.pop();
--- temp/quick-scripts/assets/Scripts/GameManager.js
+++ temp/quick-scripts/assets/Scripts/GameManager.js
@@ -1,5 +1,5 @@
-"use strict";
-cc._RF.push(module, '016acPX4gdMY7UAGkEsBhhU', 'GameManager');
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/Scripts/GameManager.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '016acPX4gdMY7UAGkEsBhhU', 'GameManager', __filename);
 // Scripts/GameManager.js
 
 "use strict";
@@ -615,3 +615,15 @@ cc.Class({
 });
 
 cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=GameManager.js.map
+        
